35c1477ecf31c72980a811bd5e3c547c03c8fb0e
yowcow5614 <133720321+yowcow5614@users.noreply.github.com>
2024-04-12 00:10:16 -0400

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,12 @@
-SELECT * FROM [master].[dbo].[Sheet1] where [i]=1
+SELECT * FROM [master].[dbo].[Sheet1] where [i] <= 100
 
+
+DECLARE @au_id char( 11 )
+
+SELECT @au_id = min( i ) FROM Sheet1
+WHILE @au_id IS NOT NULL
+
+BEGIN
+SELECT * FROM Sheet1 WHERE i = @au_id
+SELECT @au_id = min( i ) FROM Sheet1 WHERE i > @au_id
+END
